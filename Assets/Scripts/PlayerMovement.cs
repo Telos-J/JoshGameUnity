bd9b07fb3a9f371c54cd39dc8a0e0b4f3683d625
jiseungjung <roadrunner23@yonsei.ac.kr>
2020-10-22 16:31:28 +0900

    Initial game state
    1. Trump movement animation
    2. Wall collision
    3. Door control with space

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Tilemaps;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float maxSpeed;
+    public Rigidbody2D rb;
+    public Animator animator;
+    public Vector2 movement;
+    public TilemapRenderer doorSprite;
+    public TilemapCollider2D doorCollider;
+
+    // Update is called once per frame
+    // However your framerate can constantly change
+    void Update()
+    {
+        movement.Set(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        movement.Normalize();
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ToggleDoor();
+        }
+    }
+
+    // Fixed Update is executed by a fixed timer.
+    // By default, 50 times per second.
+    void FixedUpdate()
+    {
+        // Movement
+        rb.MovePosition(rb.position + movement * maxSpeed * Time.fixedDeltaTime);
+
+        if (movement.sqrMagnitude != 0f)
+        {
+            animator.SetFloat("PrevHorizontal", movement.x);
+            animator.SetFloat("PrevVertical", movement.y);
+        }
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+    }
+
+    public void ToggleDoor()
+    {
+        doorSprite.enabled = !doorSprite.enabled;
+        doorCollider.enabled = !doorCollider.enabled;
+    }
+}
