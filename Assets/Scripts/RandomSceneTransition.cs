a6c725f6cb6f5956b01d5c25e7b6c6fcacc6d06f
jiseungjung <roadrunner23@yonsei.ac.kr>
2020-11-26 16:58:35 +0900

    Add random doors

---
+++ Assets/Scripts/RandomSceneTransition.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RandomSceneTransition : MonoBehaviour
+{
+    public string[] scenesToLoad;
+    public PlayerState stateToLoad;
+    public Vector2 positionToLoad;
+    private int sceneNumber;
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.CompareTag("Player"))
+        {
+            stateToLoad.position = positionToLoad;
+            sceneNumber =  Random.Range(0, scenesToLoad.Length);
+            
+            SceneManager.LoadScene(scenesToLoad[sceneNumber]);
+        }
+    }
+}
